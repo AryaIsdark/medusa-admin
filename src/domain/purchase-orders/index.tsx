2c50a6a5a3500d73844db2f7f0c48d5f87eb4082
Arya <ars@bankingcircle.com>
2023-07-08 01:42:57 +0200

    added body card

--- src/domain/purchase-orders/index.tsx
+++ src/domain/purchase-orders/index.tsx
@@ -1,9 +1,22 @@
 import { Route, Routes } from "react-router-dom"
 import "./style.css"
 import { PurchaseOrdersSuggestionsListView } from "./purchase-orders-suggestions/purchase-order-suggestions-list-view"
+import BodyCard from "../../components/organisms/body-card"
 
 const PurchaseOrderIndex = () => {
-  return <PurchaseOrdersSuggestionsListView />
+  return (
+    <div className="flex h-full grow flex-col">
+      <div className="flex w-full grow flex-col">
+        <BodyCard
+          title="Purchase Order Suggestion"
+          subtitle="Here you can see a suggested list of purchase order based on previous day's orders"
+          className="h-fit"
+        >
+          <PurchaseOrdersSuggestionsListView />
+        </BodyCard>
+      </div>
+    </div>
+  )
 }
 
 const PurchaseOrders = () => {
