8c75a984103277d36ebb1348b199f63afc778e2e
Arya <ars@bankingcircle.com>
2023-07-08 23:23:26 +0200

    added export purchase order functionality

--- src/domain/purchase-orders/index.tsx
+++ src/domain/purchase-orders/index.tsx
@@ -1,22 +1,9 @@
 import { Route, Routes } from "react-router-dom"
 import "./style.css"
 import { PurchaseOrdersSuggestionsListView } from "./purchase-orders-suggestions/purchase-order-suggestions-list-view"
-import BodyCard from "../../components/organisms/body-card"
 
 const PurchaseOrderIndex = () => {
-  return (
-    <div className="flex h-full grow flex-col">
-      <div className="flex w-full grow flex-col">
-        <BodyCard
-          title="Purchase Order Suggestion"
-          subtitle="Here you can see a suggested list of purchase order based on previous day's orders"
-          className="h-fit"
-        >
-          <PurchaseOrdersSuggestionsListView />
-        </BodyCard>
-      </div>
-    </div>
-  )
+  return <PurchaseOrdersSuggestionsListView />
 }
 
 const PurchaseOrders = () => {
--- src/domain/purchase-orders/purchase-orders-suggestions/purchase-order-suggestions-list-view.tsx
+++ src/domain/purchase-orders/purchase-orders-suggestions/purchase-order-suggestions-list-view.tsx
@@ -1,7 +1,29 @@
 import Table from "../../../components/molecules/table"
+import BodyCard from "../../../components/organisms/body-card"
 import { useTable } from "react-table"
 import api from "../../../services/api"
 import { useEffect, useState } from "react"
+import * as XLSX from "xlsx"
+import Button from "../../../components/fundamentals/button"
+
+const exportPurchaseOrders = (jsonData) => {
+  const rearrangedData = jsonData.map((item) => ({
+    productReference: item.productReference,
+    productQuantity: item.productQuantity,
+    productName: item.productName,
+  }))
+  // Create a new workbook
+  const workbook = XLSX.utils.book_new()
+
+  // Convert JSON to worksheet
+  const worksheet = XLSX.utils.json_to_sheet(rearrangedData)
+
+  // Add the worksheet to the workbook
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1")
+
+  // Save the workbook as a file
+  XLSX.writeFile(workbook, "purchase-order.xlsx")
+}
 
 const columns = [
   {
@@ -19,6 +41,11 @@ const columns = [
     accessor: "productName",
     Cell: ({ cell: { value } }) => value,
   },
+  {
+    Header: "Stock",
+    accessor: "stock",
+    Cell: ({ cell: { value } }) => value,
+  },
 ]
 
 export const PurchaseOrdersSuggestionsListView = () => {
@@ -30,7 +57,6 @@ export const PurchaseOrdersSuggestionsListView = () => {
   })
 
   const getData = async () => {
-    console.log("I ran")
     await api.wmsPurchaseOrderSuggestions.list().then((res) => {
       setData(res.data.data)
     })
@@ -45,30 +71,47 @@ export const PurchaseOrdersSuggestionsListView = () => {
   }
 
   return (
-    <>
-      <Table>
-        <Table.Head title="Table Head">
-          {columns.map((column) => (
-            <Table.HeadCell>{column.Header}</Table.HeadCell>
-          ))}
-        </Table.Head>
-        <Table.Body>
-          {rows.map((row) => {
-            prepareRow(row)
-            return (
-              <Table.Row>
-                {row.cells.map((cell, index) => {
-                  return (
-                    <Table.Cell {...cell.getCellProps()}>
-                      {cell.render("Cell", { index })}
-                    </Table.Cell>
-                  )
-                })}
-              </Table.Row>
-            )
-          })}
-        </Table.Body>
-      </Table>
-    </>
+    <div className="flex h-full grow flex-col">
+      <div className="flex w-full grow flex-col">
+        <BodyCard
+          title="Purchase Order Suggestion"
+          subtitle="Here you can see a suggested list of purchase order based on previous day's orders"
+          className="h-fit"
+          customActionable={[
+            <Button
+              variant="primary"
+              size="small"
+              onClick={() => exportPurchaseOrders(data)}
+            >
+              Export Purchase Orders
+            </Button>,
+          ]}
+        >
+          <Table>
+            <Table.Head title="Table Head">
+              {columns.map((column) => (
+                <Table.HeadCell>{column.Header}</Table.HeadCell>
+              ))}
+            </Table.Head>
+            <Table.Body>
+              {rows.map((row) => {
+                prepareRow(row)
+                return (
+                  <Table.Row>
+                    {row.cells.map((cell, index) => {
+                      return (
+                        <Table.Cell {...cell.getCellProps()}>
+                          {cell.render("Cell", { index })}
+                        </Table.Cell>
+                      )
+                    })}
+                  </Table.Row>
+                )
+              })}
+            </Table.Body>
+          </Table>
+        </BodyCard>
+      </div>
+    </div>
   )
 }
